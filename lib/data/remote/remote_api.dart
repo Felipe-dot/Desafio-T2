f64a3d67a19e5802759880238cadca2ce8f783d5
Felipe-dot <felipeluis610@gmail.com>
2023-11-21 16:09:56 -0300

    feat: return data from the api

--- lib/data/remote/remote_api.dart
+++ lib/data/remote/remote_api.dart
@@ -3,12 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class RemoteApi {
-  Future<void> fetchData() async {
+  Future<List<dynamic>> fetchData() async {
     final response =
         await http.get(Uri.parse('https://escribo.com/books.json'));
 
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
+      return data;
     } else {
       throw Exception('Falha na requisição');
     }
