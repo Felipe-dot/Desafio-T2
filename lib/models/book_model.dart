ef17a5f23e9190e5db243640255aa9ce9d29f4be
Felipe-dot <felipeluis610@gmail.com>
2023-11-21 11:07:55 -0300

    feat: Add book model

---
+++ lib/models/book_model.dart
@@ -0,0 +1,15 @@
+class BookModel {
+  final int id;
+  final String title;
+  final String author;
+  final String coverUrl;
+  final String downloadUrl;
+
+  BookModel({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.coverUrl,
+    required this.downloadUrl,
+  });
+}
